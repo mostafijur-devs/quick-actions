89e3cc346febe38909a56a0c1203844208a222db
Mostafijur Rahman <mostafijur.devs@gmail.com>
2025-08-19 13:02:04 +0600

    use quick action package

--- lib/screen/frist_screen.dart
+++ lib/screen/frist_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class FristScreen extends StatelessWidget {
+  const FristScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quick_action/screen/secend_screen.dart';
 import 'package:quick_actions/quick_actions.dart';
+
+import 'frist_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,28 +13,51 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   QuickActions quickActions = QuickActions();
-  
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _quickAction();
   }
+
   void _quickAction() {
-    quickActions.initialize()
+    quickActions.setShortcutItems(<ShortcutItem>[
+      ShortcutItem(type: 'frist_screen', localizedTitle: 'Go To Frist Screen',icon: 'ic_launcher'),
+      ShortcutItem(
+        type: 'secend_screen',
+        localizedTitle: 'Go To Second Screen',
+        icon:'images',
+        localizedSubtitle: 'open second screen'
+      ),
+    ]);
+    quickActions.initialize((type) {
+      if (type != null) {
+        if (type == 'frist_screen') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FristScreen()),
+          );
+        } else if (type == 'secend_screen') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecendScreen()),
+          );
+        }
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-        centerTitle: true,
-      ),
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page'), centerTitle: true),
       body: Center(
-        child: Text('This is homeo page', style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+        child: Text(
+          'This is homeo page',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
-
-  
 }
