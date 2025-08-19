8d7a92658df15fbd8701e4d16e9989f16a3153f4
Mostafijur Rahman <mostafijur.devs@gmail.com>
2025-08-19 13:10:34 +0600

    quick action add home page

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -23,22 +23,35 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _quickAction() {
     quickActions.setShortcutItems(<ShortcutItem>[
-      ShortcutItem(type: 'frist_screen', localizedTitle: 'Go To Frist Screen',icon: 'ic_launcher'),
+      ShortcutItem(type: 'homePage', localizedTitle: 'Home Page', icon: 'home',localizedSubtitle: 'this is home page'),
+
+      ShortcutItem(
+        type: 'frist_screen',
+        localizedTitle: 'Go To Frist Screen',
+        icon: 'ic_launcher',
+      ),
       ShortcutItem(
         type: 'secend_screen',
         localizedTitle: 'Go To Second Screen',
-        icon:'images',
-        localizedSubtitle: 'open second screen'
+        icon: 'images',
+        localizedSubtitle: 'open second screen',
       ),
     ]);
     quickActions.initialize((type) {
       if (type != null) {
-        if (type == 'frist_screen') {
+
+        if (type == 'homePage') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomeScreen()),
+          );
+        } else if (type == 'frist_screen') {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => FristScreen()),
           );
-        } else if (type == 'secend_screen') {
+        }
+        else if (type == 'secend_screen') {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => SecendScreen()),
